cf18be2ec6b16f8feb791fad988b0c4eb9b1bcf3
Musheer <musheeran165@gmail.com>
2024-11-18 23:51:18 +0530

    push add files

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const globalStyles =StyleSheet.create({
+    btn_primary:{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        borderWidth: 1,
+        borderBlockColor: 'red',
+        padding: 3,
+        marginBlock:4
+    }
+})
+export default globalStyles
